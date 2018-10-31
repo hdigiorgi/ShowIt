0bd8618e9d7b21e2abc7dc1bf6bb40d72707bab3
Hernan Di Giorgi <me@hdigiorgi.com>
2018-10-31 13:41:22 -0300

    remove console.log

--- public/javascripts/landing.js
+++ public/javascripts/landing.js
@@ -1,8 +1,6 @@
 $(() => {
     $(".landing-image-grid-container .cols").on("touchstart touchend", function(e){
       const target = $(e.currentTarget).children(".title")
-      window.t = target
-      console.log(target)
       $(target).toggleClass("title-hover");
     });
 })
